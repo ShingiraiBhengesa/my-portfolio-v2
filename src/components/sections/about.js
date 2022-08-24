eb1a76ded846f3927e4d553089f95fbd76c922d1
shingie <72065045+ShingiraiBhengesa@users.noreply.github.com>
2022-08-24 20:39:34 +0300

    update images

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -166,7 +166,7 @@ const About = () => {
           <div className="wrapper">
             <StaticImage
               className="img"
-              src="../../images/me.jpg"
+              src="../../images/shingie.jpg"
               width={500}
               quality={95}
               formats={['AUTO', 'WEBP', 'AVIF']}
